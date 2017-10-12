a4c9cc014333d4f1873548f2b015e0c8eb9b0b4f
gene-yoo <gene.y.yoo@gmail.com>
2017-10-11 23:35:41 -0400

    Lab complete

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Recipe.create(name: "Test Recipe 1", ingredients: "Test Ingredients 1", cook_time: "1 hour")
+Recipe.create(name: "Test Recipe 2", ingredients: "Test Ingredients 2", cook_time: "2 hours")
